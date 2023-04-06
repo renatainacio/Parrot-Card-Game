188dbb6be13375c459d7edcc7dc6aecc5deb8a70
Renata Inacio <revazgauska@gmail.com>
2023-04-06 20:16:04 -0300

    add comportamento clique das cartas

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let numeroCartas;
+let numeroCartas = 0;
+let cartasViradasJogada = 0;
+let jogadas = 0;
+let primeiraCarta;
+let segundaCarta;
+let pontos = 0;
 
-while (numeroCartas < 2 || numeroCartas % 2 != 0 || numeroCartas > 14)
-   numeroCartas = prompt("Informe a quantidade de cartas (min:2, max:14):");
+while (numeroCartas < 4 || numeroCartas % 2 != 0 || numeroCartas > 14)
+   numeroCartas = prompt("Informe a quantidade de cartas (min:4, max:14):");
 
 const gifs = ["img/bobrossparrot.gif",
                "img/explodyparrot.gif",
@@ -27,7 +32,7 @@ function comparador() {
 
 for (i = 0; i < numeroCartas; i++) {
    document.querySelector(".cards").innerHTML += `
-   <div class="card" onclick="showCard(this)">
+   <div class="card" onclick="clickCard(this)">
       <div class="front-face face">
          <img src="img/back.png"/>
       </div>
@@ -38,7 +43,36 @@ for (i = 0; i < numeroCartas; i++) {
    `
 }
 
-function showCard(card){
+function clickCard(card){
+   if (cartasViradasJogada <= 1) {
+      flipCard(card);
+      if (cartasViradasJogada == 0)
+         primeiraCarta = card;
+      else
+         segundaCarta = card;
+      cartasViradasJogada++;
+   }
+   if (cartasViradasJogada == 2) {
+      if(primeiraCarta.querySelector(".back-face").innerHTML == segundaCarta.querySelector(".back-face").innerHTML)
+      {
+         pontos += 2;
+         cartasViradasJogada = 0;
+         jogadas++;
+         console.log(pontos);
+      }   
+      else
+         setTimeout(notAMatch, 1000);
+   }
+}
+
+function notAMatch() {
+   flipCard(primeiraCarta);
+   flipCard(segundaCarta);
+   cartasViradasJogada = 0;
+   jogadas++;
+}
+
+function flipCard(card){
    card.querySelector(".front-face").classList.toggle("front");
    card.querySelector(".back-face").classList.toggle("back");
 }
